5aa5b27c54a5fb2106324d6c28ad730c04c1c251
Nick <nikolaj.sprinc@gmail.com>
2023-06-26 13:29:11 +0300

    Startt

---
+++ script.js
@@ -0,0 +1,8 @@
+// Отримання посилань на елементи
+const navbarCollapse = document.getElementById('navbarSupportedContent');
+const navbarToggler = document.querySelector('.navbar-toggler');
+
+// Додавання обробника подій для натискання на бургер
+navbarToggler.addEventListener('click', function() {
+  navbarCollapse.classList.toggle('show');
+});
