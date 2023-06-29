63a456b42a3b071afbce746abf2a4a8ca7eb2f88
Nicholas853 <102964416+Nicholas853@users.noreply.github.com>
2023-06-30 01:40:05 +0300

    Carusel

--- script.js
+++ script.js
@@ -7,6 +7,48 @@ navbarToggler.addEventListener('click', function() {
   navbarCollapse.classList.toggle('show');
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+  var carouselContainer = document.querySelector(".carousel-container");
+  var carouselItems = carouselContainer.querySelectorAll(".carousel-item");
+  var carouselArrowLeft = carouselContainer.querySelector(".left-arrow");
+  var carouselArrowRight = carouselContainer.querySelector(".right-arrow");
+  var activeIndex = 0;
+
+  carouselArrowLeft.addEventListener("click", function() {
+    rotateCarousel("left");
+  });
+
+  carouselArrowRight.addEventListener("click", function() {
+    rotateCarousel("right");
+  });
+
+  showInitialItems();
+
+  function showInitialItems() {
+    carouselItems[activeIndex].classList.add("visible-left");
+    carouselItems[(activeIndex + 1) % carouselItems.length].classList.add("active");
+    carouselItems[(activeIndex + 2) % carouselItems.length].classList.add("visible-right");
+  }
+
+  function rotateCarousel(direction) {
+    carouselItems.forEach(function(item) {
+      item.classList.remove("active", "visible-left", "visible-right");
+    });
+
+    if (direction === "left") {
+      activeIndex = (activeIndex - 1 + carouselItems.length) % carouselItems.length;
+    } else {
+      activeIndex = (activeIndex + 1) % carouselItems.length;
+    }
+
+    carouselItems[(activeIndex - 1 + carouselItems.length) % carouselItems.length].classList.add("visible-left");
+    carouselItems[activeIndex].classList.add("active");
+    carouselItems[(activeIndex + 1) % carouselItems.length].classList.add("visible-right");
+  }
+});
+
+
+
 
 
 
